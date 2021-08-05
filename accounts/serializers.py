14480d3e0d7a266ebaa2d02be4fbf7c91269f2e0
Renan  Sampaio <renansampaio0401@yahoo.com>
2021-08-04 20:37:12 -0400

    feat(initial project): checkpoint 1 made

---
+++ accounts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    is_staff = serializers.BooleanField(required=False)
+    is_superuser = serializers.BooleanField(required=False)
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
